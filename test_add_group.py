daac95c328850606ed3f1adcb35dab2038ec1059
Max Davion <7473233@gmail.com>
2015-03-02 21:08:42 +0300

    Выделение  действий в тесте в отдельные методы

--- test_add_group.py
+++ test_add_group.py
@@ -10,15 +10,15 @@ def is_alert_present(wd):
     except:
         return False
 
-class create_group(unittest.TestCase):
+class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-    
-    def test_create_group(self):
-        success = True
-        wd = self.wd
+
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -26,8 +26,14 @@ class create_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd):
+        # Init group greation
         wd.find_element_by_name("new").click()
+        # Fill group create group
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("groupname")
@@ -37,10 +43,24 @@ class create_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("groupfooter")
+        #Submin group creation
         wd.find_element_by_name("submit").click()
+
+    def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
-        self.assertTrue(success)
-    
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def test_create_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
     def tearDown(self):
         self.wd.quit()
 
