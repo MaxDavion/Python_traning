ea6550ec04613ed75d8d2bf44f21ad167817c984
Max Davion <7473233@gmail.com>
2015-03-03 21:00:50 +0300

    Создан объект Group и из тестов вынесен объект группа

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
+from group import Group
 import time, unittest
 
 def is_alert_present(wd):
@@ -29,19 +30,19 @@ class test_add_group(unittest.TestCase):
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd, group_name, group_header, group_footer):
+    def create_group(self, wd, group):
         # Init group greation
         wd.find_element_by_name("new").click()
         # Fill group create group
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group_name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group_header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group_footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         #Submin group creation
         wd.find_element_by_name("submit").click()
 
@@ -56,7 +57,7 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, user="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, "groupname", "groupheader", "groupfooter")
+        self.create_group(wd, Group("groupname", "groupheader", "groupfooter"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -65,7 +66,7 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, user="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, "", "", "")
+        self.create_group(wd, Group("", "", ""))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
