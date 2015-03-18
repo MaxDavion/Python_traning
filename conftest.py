673daa5c3d65e0d5d806414a787cefbb6ca1d861
Max Davion <7473233@gmail.com>
2015-03-18 19:33:32 +0300

    Сделан интеллектуальный login

--- conftest.py
+++ conftest.py
@@ -5,20 +5,19 @@ fixture = None
 
 @pytest.fixture
 def app(request):
+    global fixture
     if fixture is None:
-        global fixture
         fixture = Application()
-        fixture.session.login(user="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.login(user="admin", password="secret")
+    fixture.session.ensure_login(user="admin", password="secret")
     return fixture
 
 @pytest.fixture(scope = 'session', autouse = True)
 def stop(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
