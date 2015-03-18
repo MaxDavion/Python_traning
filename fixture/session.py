84418ad753d210b7644eba55eced8adfec64f3ac
Max Davion <7473233@gmail.com>
2015-03-18 19:45:35 +0300

    Revert "Сделан интеллектуальный login"
    This reverts commit 673daa5c3d65e0d5d806414a787cefbb6ca1d861.

--- fixture/session.py
+++ fixture/session.py
@@ -18,33 +18,7 @@ class SessionHelper:
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
-    def is_logged_in(self):
-        wd = self.app.wd
-        return len(wd.find_elements_by_link_text("Logout")) > 0
-
-    def is_logged_in_as(self, user):
-        wd = self.app.wd
-        return wd.find_element_by_xpath("//*[@id='top']/form/b").text == "(" + user + ")"
-
-    def ensure_login(self, user, password):
-        wd = self.app.wd
-        if self.is_logged_in():
-            if is_logged_in_as(user):
-                return
-            else:
-                self.logout()
-        self.login(user, password)
-
-
-
-
-
-
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def ensure_logout(self):
-        wd = self.app.wd
-        if self.is_logged_in():
-            self.logout()
