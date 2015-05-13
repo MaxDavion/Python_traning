e30dd479a08553d630928ffc543eb8d65dacf685
Max Davion <7473233@gmail.com>
2015-05-13 22:14:56 +0300

    Задание №21
    Файл test_db_matches_ui.py

--- fixture/db.py
+++ fixture/db.py
@@ -20,7 +20,7 @@ class DbFixture:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor:
                 (id, name, header, footer) = row
-                list.append(Group(id=id, name=name, header=header, footer=footer))
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
         finally:
             cursor.close()
         return list
@@ -29,14 +29,13 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated = '0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, phone2, email, email2, email3 from addressbook where deprecated = '0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname)  = row
-                list.append(Contact(id=id, firstname=firstname, lastname=lastname))
+                (id, firstname, lastname, address, home, mobile, work, phone2, email, email2, email3) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address, all_phones_from_home_page=("%s\n%s\n%s\n%s" % (home, mobile, work, phone2)), all_emails_from_home_page=("%s\n%s\n%s" % (email, email2, email3))))
         finally:
             cursor.close()
         return list
-        list = []
 
     def destroy(self):
         self.connection.close()
