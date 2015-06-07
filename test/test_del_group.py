2ed40e70a5ad94c269237dc31f49732af3887dec
Davion <7473233@gmail.com>
2015-06-07 13:35:55 +0300

    allure4

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -11,10 +11,10 @@ def test_delete_random_group(app, db, check_ui):
             app.group.create(Group(name="group for delete"))
     with pytest.allure.step('Оракул. Получаем список групп, до выполнения действия'):
         old_groups = db.get_group_list()
-    with pytest.allure.step('Выполняем действие удаления случайной группы %s'):
+    with pytest.allure.step('Выполняем действие удаления случайной группы'):
         group = random.choice(old_groups)
         app.group.delete_by_id(group.id)  #
-    with pytest.allure.step('Проверяему что группа удалена %s' % group):
+    with pytest.allure.step('Проверяему что группа удалена'):
         new_groups = db.get_group_list()  #
         old_groups.remove(group)  # Оракул. Удаляем из оракула удаленную через интерфейс группу
         assert old_groups == new_groups
